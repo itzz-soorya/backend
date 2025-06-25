f990c8b4d869d84f9faa152eb8f771e7d6e71cdc
itzz-soorya <23cs116@nandhaengg.org>
2025-06-25 10:04:59 +0530

    admin user page request done

--- turfmanagement/Controllers/AdminUserController.cs
+++ turfmanagement/Controllers/AdminUserController.cs
@@ -27,6 +27,7 @@ namespace turfmanagement.Controllers
 
             string query = @"
                 SELECT 
+                    u.UserId,
                     u.Name,
                     u.PhoneNumber,
                     u.LastBookingDate,
@@ -34,29 +35,41 @@ namespace turfmanagement.Controllers
                         SELECT MIN(b.BookingDate)
                         FROM Bookings b
                         WHERE b.UserId = u.UserId AND b.BookingDate > CURRENT_DATE
-                    ) AS UpcomingBooking
+                    ) AS UpcomingBooking,
+                    (
+                        SELECT COUNT(*) 
+                        FROM Bookings b 
+                        WHERE b.UserId = u.UserId
+                    ) AS TotalBookings
                 FROM Users u;
             ";
 
             using var cmd = new NpgsqlCommand(query, conn);
             using var reader = cmd.ExecuteReader();
 
+            int index = 1;
             while (reader.Read())
             {
                 string lastBooking = reader["LastBookingDate"] is DBNull
-                    ? "N/A"
+                    ? "-"
                     : Convert.ToDateTime(reader["LastBookingDate"]).ToString("dd/MM/yyyy");
 
                 string upcoming = reader["UpcomingBooking"] is DBNull
-                    ? "Not yet"
+                    ? "-"
                     : Convert.ToDateTime(reader["UpcomingBooking"]).ToString("dd/MM/yyyy");
+
+                int totalBookings = reader["TotalBookings"] is DBNull
+                    ? 0
+                    : Convert.ToInt32(reader["TotalBookings"]);
 
                 result.Add(new UserDetailDto
                 {
+                    No = index++,
                     Name = reader["Name"].ToString(),
                     PhoneNumber = reader["PhoneNumber"].ToString(),
                     LastBooking = lastBooking,
-                    UpcomingBooking = upcoming
+                    UpcomingBooking = upcoming,
+                    TotalBookings = totalBookings
                 });
             }
 
@@ -66,9 +79,11 @@ namespace turfmanagement.Controllers
 
     public class UserDetailDto
     {
+        public int No { get; set; }  // serial index
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string LastBooking { get; set; }
         public string UpcomingBooking { get; set; }
+        public int TotalBookings { get; set; }
     }
 }
